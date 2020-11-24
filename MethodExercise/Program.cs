a97459ef5b25dec0ad9f15c2c624efb391769ae1
ZodyT <Zoraida1988@yahoo.com>
2020-11-24 09:24:59 -0600

    Finished

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+           
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+           
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+           Console.WriteLine($"So {name} you like {color} {animal}s that listen to {band}.");
+
+        }
+    }
+}
+
+
+    
